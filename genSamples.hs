112ff0482fbba936f0303bb1b6f5ff4f552d2264
segfault <s9gf4ult@gmail.com>
2013-09-02 15:17:32 +0600

    fixed

--- genSamples.hs
+++ genSamples.hs
@@ -27,30 +27,31 @@ printHelp = putStrLn "usage: genSamples <low data> <high data> <low X> <high X>"
 writeFiles :: [(FilePath, FullData)] -> IO ()
 writeFiles x = forM_ x $ \(fp, fd) -> B.writeFile fp $ runPut $ genFullData fd
 
-genFiles :: Word16 -> Word16 -> Word16 -> Word16 -> [(FilePath, FullData)]
-genFiles lowd hid lowx hix = do
+genFiles :: Word8 -> Word8 -> Word16 -> Word16 -> Word16 -> Word16 -> [(FilePath, FullData)]
+genFiles lowt hit lowd hid lowx hix = do
+  t <- [lowt..hit]
   x@(a, b, c, d) <- grp4 [lowx..hix]
-  return (show a ++ "-" ++ show d ++ ".dtm", gfd [lowd..hid] x)
+  return (show t ++ "." ++ show a ++ "-" ++ show d ++ ".dtm", gfd [lowd..hid] x t)
   where
-    gfd d x = FullData (ghdr x) (gsns d)
-    ghdr (x1, x2, x3, x4) = Header { hMagicWords = (2, 2, 1)
-                                   , hStartDate = (DateTime 10 10 10 10 10 10)
-                                   , hEndDate = (DateTime 10 10 10 10 11 10)
-                                   , hPlot = ""
-                                   , hProduct = ""
-                                   , hDiameter = 720
-                                   , hThickness = 14 --  FIXME: generate
-                                   , hBlock = 129
-                                   , hTemperature = 27
-                                   , hLength = 1248
-                                   , hProdNumber = 205
-                                   , hArg1 = x1
-                                   , hArg2 = x2
-                                   , hArg3 = x3
-                                   , hArg4 = x4
-                                   , hCommonArg = 5920
-                                   , hCheckSum = 0
-                                   , hCalcualtedChSum = 0 }
+    gfd d x t = FullData (ghdr t x) (gsns d)
+    ghdr th (x1, x2, x3, x4) = Header { hMagicWords = (2, 2, 1)
+                                      , hStartDate = (DateTime 10 10 10 10 10 10)
+                                      , hEndDate = (DateTime 10 10 10 10 11 10)
+                                      , hPlot = ""
+                                      , hProduct = ""
+                                      , hDiameter = 720
+                                      , hThickness = th --  FIXME: generate
+                                      , hBlock = 129
+                                      , hTemperature = 27
+                                      , hLength = 1248
+                                      , hProdNumber = 205
+                                      , hArg1 = x1
+                                      , hArg2 = x2
+                                      , hArg3 = x3
+                                      , hArg4 = x4
+                                      , hCommonArg = 5920
+                                      , hCheckSum = 0
+                                      , hCalcualtedChSum = 0 }
     gsns a = Sensors $ map (\n -> (n,n,n,n)) a
 
 
@@ -58,14 +59,16 @@ main :: IO ()
 main = do
   a <- getArgs
   case a of
-    [lx, hx, llx, hhx] -> do
+    [lt, ht, lx, hx, llx, hhx] -> do
       let lowd = read lx
           hid = read hx
           lowx = read llx
           hix = read hhx
+          lowt = read lt
+          hit = read ht
       when (lowd >= hid) $ fail "lowd must be < hid"
       when (lowx >= hix) $ fail "lowx must be < hix"
-      writeFiles $ genFiles lowd hid lowx hix
+      writeFiles $ genFiles lowt hit lowd hid lowx hix
 
     _ -> printHelp
 
