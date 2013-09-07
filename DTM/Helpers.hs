c999a25677d91e274053585c3cbedad6ffa3df41
segfault <s9gf4ult@gmail.com>
2013-09-07 19:57:24 +0600

    range parser

--- DTM/Helpers.hs
+++ DTM/Helpers.hs
@@ -2,6 +2,9 @@
 module DTM.Helpers where
 
 
+import Control.Monad.Random
+import Control.Applicative
+import Control.Monad
 import DTM.Generator
 import DTM.Parser
 import DTM.Types
@@ -10,7 +13,7 @@ import Data.Time
 import Data.Word
 import Data.Ratio
 import Data.Attoparsec.Text.Lazy  
-import Data.List (zip4, genericLength)
+import Data.List (zip4, genericLength, foldl')
 import System.IO
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
@@ -21,6 +24,21 @@ data SinComponent = SinComponent
                     { scPeriod :: Double
                     , scAmplitude :: Double }
                   deriving (Show, Eq)
+
+data CompRange = CompRange
+                 { crCount :: Int
+                 , crStart :: SinComponent
+                 , crEnd :: SinComponent
+                 }
+               | CompRepeat
+                 { crCount :: Int
+                 , crStart :: SinComponent
+                 }
+               deriving (Show, Eq)
+
+data Digit a = Digit a
+             | DRange a a
+             deriving (Show, Eq)
 
 
 
@@ -88,20 +106,60 @@ genData (InitialData
               (map round $ genX y3 z3)
               (map round $ genX y4 z4)
                                           
-parseComp :: TL.Text -> [SinComponent]
+parseComp :: TL.Text -> [CompRange]
 parseComp t = case eitherResult $ parse compParser t of
   Left e -> error "sinus component must be in format: period:amplitude,period:amplitude..."
   Right r  -> r
 
-compParser :: Parser [SinComponent]
-compParser = sepBy1' oneComp (skipSpace >> (char ',') >> skipSpace)
+digitParser :: Parser a -> Parser (Digit a)
+digitParser d = dRange <|> (Digit <$> d)
   where
+    dRange = do
+      a <- d
+      (skipSpace >> char '-' >> skipSpace)
+      b <- d
+      return $ DRange a b
+
+compParser :: Parser [CompRange]
+compParser = sepBy1' comp (skipSpace >> (char ',') >> skipSpace)
+  where
+    comp = manyComp <|> oneComp 
+    manyComp = do
+      c <- oneComp
+      (skipSpace >> char 'x' >> skipSpace)
+      v <- decimal
+      return $ c {crCount = v}
     oneComp = do
-      a <- rational
+      a <- digitParser rational
       skipSpace >> (char ':') >> skipSpace
-      b <- rational
-      return $ SinComponent a b
+      b <- digitParser rational
+      return $ digToComp a b
+    digToComp (DRange a b) (DRange c d) = CompRange 1 (SinComponent a c) (SinComponent b d)
+    digToComp (Digit a) (Digit b) = CompRepeat 1 $ SinComponent a b
+    digToComp (Digit a) (DRange b c) = CompRange 1 (SinComponent a b) (SinComponent a c)
+    digToComp (DRange a b) (Digit c) = CompRange 1 (SinComponent a c) (SinComponent b c)
+    
 
-randomSensors :: [SinComponent] -> [SinComponent] -> Rand (Double, Double, Double, Double)
-randomSensors com spec = undefined
-
+randomSensors :: (RandomGen g) => Int -> Double -> [CompRange] -> [CompRange] -> Rand g ([Double], [Double], [Double], [Double])
+randomSensors len hi comR specR = do
+  com <- concat <$> mapM rangeToComp comR
+  spec <- concat <$> mapM rangeToComp specR
+  cphase <- mapM newPhase com
+  let cdata = genSins $ zip cphase com
+  phasea <- mapM newPhase spec
+  phaseb <- mapM newPhase spec
+  phasec <- mapM newPhase spec
+  phased <- mapM newPhase spec
+  return (map (+hi) $ zipWith (+) cdata $ genSins $ zip phasea spec,
+          map (+hi) $ zipWith (+) cdata $ genSins $ zip phaseb spec,
+          map (+hi) $ zipWith (+) cdata $ genSins $ zip phasec spec,
+          map (+hi) $ zipWith (+) cdata $ genSins $ zip phased spec)
+  where
+    newPhase (SinComponent p _) = getRandomR (0, p*2)
+    genSins p = foldl' (zipWith (+)) (replicate ((div len 2)+1) 0) $ map genSin p
+    genSin (ph, (SinComponent per ampl)) = map (\x -> ampl * (sin ((x/per*2*pi) + ph))) [0,2..fromIntegral len]
+    rangeToComp (CompRepeat i c) = return $ replicate i c
+    rangeToComp (CompRange i (SinComponent a b) (SinComponent c d)) = replicateM i $ do
+      x <- getRandomR (a, c)
+      y <- getRandomR (b, d)
+      return $ SinComponent x y
--- genData.hs
+++ genData.hs
@@ -4,56 +4,61 @@
 
 module Main where
 
-import Control.Monad
 import Control.Applicative
+import Control.Monad
+import Control.Monad.Random
 import DTM.Generator
 import DTM.Helpers
 import DTM.Types
 import Data.Fixed
-import Data.Serialize
 import Data.List (genericLength, zip4)
+import Data.Monoid
+import Data.Maybe
+import Data.Ratio
+import Data.Serialize
 import Data.Time
 import Data.Word
-import Data.Ratio
-import System.Environment
 import System.Console.GetOpt
+import System.Environment
+import System.Random
 import qualified Data.ByteString as B
 import qualified Data.Text.Lazy as T
 
 data COption = CHelp
-             | CommonComp [SinComponent]
-             | SpecComp [SinComponent]
-             | High Double
+             | CommonComp [CompRange]
+             | SpecComp [CompRange]
+             | Thickness Double
              deriving (Show, Eq)
 
 data DOption = DOption
-               { dCommon :: [SinComponent]
-               , dSpec :: [SinComponent]
-               , dHigh :: Maybe Double
+               { dCommon :: [CompRange]
+               , dSpec :: [CompRange]
+               , dHigh :: Double
                }
+               deriving (Show, Eq)
 
 mergeOptions :: [COption] -> DOption
 mergeOptions x = DOption
                  { dCommon = concat $ cmn x
                  , dSpec = concat $ spec x
-                 , dHigh = hi x
+                 , dHigh = fromMaybe 0 $ hi x
                  }
   where
     cmn [] = []
-    cmn (CommonComp x):xs = x:(cmn xs)
-    cmn _:xs = cmn xs
+    cmn ((CommonComp x):xs) = x:(cmn xs)
+    cmn (_:xs) = cmn xs
     spec [] = []
-    spec (SpecComp x):xs = x:(spec xs)
-    spec _:xs = spec xs
-    hi x = getLast $ map (Last . gethi) x
-    gethi (High d) = Just d
+    spec ((SpecComp x):xs) = x:(spec xs)
+    spec (_:xs) = spec xs
+    hi x = getLast $ mconcat $ map (Last . gethi) x
+    gethi (Thickness d) = Just d
     gethi _ = Nothing
 
 optionsDescr :: [OptDescr COption]
 optionsDescr = [ Option "h" ["help"] (NoArg CHelp) "show this message"
                , Option "c" ["common"] (ReqArg (CommonComp . parseComp . T.pack) "arg") "common sinus component"
                , Option "s" ["specific"] (ReqArg (SpecComp . parseComp . T.pack) "arg") "specific sinus component"
-               , Option "H" ["high"] (ReqArg (High . read) "arg") "initial high of generated values, default is 0"
+               , Option "t" ["thickness"] (ReqArg (Thickness . read) "arg") "initial high of generated values, default is 0"
                ]
 
 
@@ -64,5 +69,10 @@ main = do
     _ -> do
       g <- newStdGen
       let mopts = mergeOptions opts
-      let sns = evalRand (randomSensors (dCommon mopts) (dSpec mopts)) g
       
+      let (a, b, c, d) = evalRand (randomSensors 500 (dHigh mopts) (dCommon mopts) (dSpec mopts)) g
+      B.writeFile "out.dtm" $ runPut $ genFullData $ genData
+        $ InitialData (470, 470, 470, 470)
+        (map toRational a, map toRational b, map toRational c, map toRational d)
+        $ Header (2, 2, 1) (DateTime 0 0 0 0 0 0) (DateTime 0 0 0 0 0 0) "" "" 500 17
+        250 27 0 205 0 0 0 0 0 0 0
